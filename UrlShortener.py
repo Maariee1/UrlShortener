40beefef8bec34940f5dbc57a4c70913bbdde62b
Maariee1 <ciaramariecondino0901@gmail.com>
2024-12-04 20:41:37 +0800

    Update UrlShortener.py

--- UrlShortener.py
+++ UrlShortener.py
@@ -37,27 +37,27 @@ def MainTab():
         elif selected_value == '3':
              BlankPage3()
             
-    # #Syntax to add image using Pil or pillow
-    # image = Image.open("GURL BG (2).png")
-    # image = image.resize((150, 100))
-    # photo = ImageTk.PhotoImage(image)
+    #Syntax to add image using Pil or pillow
+    image = Image.open("GURL LOGO.png")
+    image = image.resize((150, 100))
+    photo = ImageTk.PhotoImage(image)
 
-    # style = ttk.Style()
-    # style.configure("Custom.TLabel", background='#FBF4C4')
-    # label = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
-    # label.place(x=545, y=580)
-    # label.image = photo
+    style = ttk.Style()
+    style.configure("Custom.TLabel", background='#FBF4C4')
+    label = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
+    label.place(x=545, y=580)
+    label.image = photo
     
-    # #Gurl and quote
-    # image = Image.open("C:\\Users\\Ciara\\Downloads\\GURL BG (6 x 2 in) (1).png")
-    # image = image.resize((1270, 400))
-    # photo = ImageTk.PhotoImage(image)
+    #Gurl and quote
+    image = Image.open("GURL QUOTE.png")
+    image = image.resize((1270, 400))
+    photo = ImageTk.PhotoImage(image)
 
-    # style = ttk.Style()
-    # style.configure("Custom.TLabel", background='#FBF4C4')
-    # label1 = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
-    # label1.pack()
-    # label1.image = photo
+    style = ttk.Style()
+    style.configure("Custom.TLabel", background='#FBF4C4')
+    label1 = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
+    label1.pack()
+    label1.image = photo
     
     #GENERATE LINK
     label = customtkinter.CTkLabel(window,
@@ -232,7 +232,7 @@ def AboutUsButton():
         widget.destroy()
     
     #Quote of GURL    
-    image = Image.open("C:\\Users\\Ciara\\Downloads\\GURL BG (6 x 2 in) (2).png")
+    image = Image.open("GURL BIG QUOTE.png")
     image = image.resize((500, 200))
     photo = ImageTk.PhotoImage(image)
 
@@ -243,7 +243,7 @@ def AboutUsButton():
     label.image = photo
     
     #LOGO of GURL    
-    image = Image.open("C:\\Users\\Ciara\\PYTHON\\UrlShortener\\GURL BG (2).png")
+    image = Image.open("GURL LOGO.png")
     image = image.resize((150, 100))
     photo = ImageTk.PhotoImage(image)
 
@@ -322,6 +322,28 @@ def AboutUsButton():
 def BlankPage2():  
     for widget in window.winfo_children():
         widget.destroy()
+        
+    #Syntax to add image using Pil or pillow
+    image = Image.open("GURL LOGO.png")
+    image = image.resize((150, 120))
+    photo = ImageTk.PhotoImage(image)
+
+    style = ttk.Style()
+    style.configure("Custom.TLabel", background='#FBF4C4')
+    label = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
+    label.place(x=535, y=595)
+    label.image = photo
+    
+    #Gurl and quote
+    image = Image.open("GURL QUOTE.png")
+    image = image.resize((1290, 450))
+    photo = ImageTk.PhotoImage(image)
+
+    style = ttk.Style()
+    style.configure("Custom.TLabel", background='#FBF4C4')
+    label1 = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
+    label1.pack()
+    label1.image = photo    
         
     def generateLink():
         print("Generate functionality to be implemented.")
@@ -498,6 +520,17 @@ def BlankPage2():
 def BlankPage3():  
     for widget in window.winfo_children():
         widget.destroy()
+    
+    #Gurl and quote
+    image = Image.open("GURL QUOTE.png")
+    image = image.resize((1290, 450))
+    photo = ImageTk.PhotoImage(image)
+
+    style = ttk.Style()
+    style.configure("Custom.TLabel", background='#FBF4C4')
+    label1 = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
+    label1.pack()
+    label1.image = photo  
         
     def generateLink3():
         username = entryP31.get(), entryP32.get(), entryP33.get()
