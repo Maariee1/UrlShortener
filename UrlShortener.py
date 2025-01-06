56bf53cba229d8fc1800d1a9e3a22bc9ccc3df2a
Maariee1 <ciaramariecondino0901@gmail.com>
2025-01-07 01:30:22 +0800

    Update UrlShortener.py

--- UrlShortener.py
+++ UrlShortener.py
@@ -92,26 +92,26 @@ def MainTab():
              BlankPage3()
             
     #Syntax to add image using Pil or pillow
-    #image = Image.open("GURL LOGO.png")
-    #image = image.resize((150, 100))
-    #photo = ImageTk.PhotoImage(image)
+    image = Image.open("GURL LOGO.png")
+    image = image.resize((150, 100))
+    photo = ImageTk.PhotoImage(image)
 
-    #style = ttk.Style()
-    #style.configure("Custom.TLabel", background='#FBF4C4')
-    #label = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
-    #label.place(x=545, y=580)
-    #label.image = photo
+    style = ttk.Style()
+    style.configure("Custom.TLabel", background='#FBF4C4')
+    label = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
+    label.place(x=545, y=580)
+    label.image = photo
     
     #Gurl and quote
-    #image = Image.open("GURL QUOTE.png")
-    #image = image.resize((1270, 400))
-    #photo = ImageTk.PhotoImage(image)
+    image = Image.open("GURL QUOTE.png")
+    image = image.resize((1270, 400))
+    photo = ImageTk.PhotoImage(image)
 
-    #style = ttk.Style()
-    #style.configure("Custom.TLabel", background='#FBF4C4')
-    #label1 = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
-    #label1.pack()
-    #label1.image = photo
+    style = ttk.Style()
+    style.configure("Custom.TLabel", background='#FBF4C4')
+    label1 = ttk.Label(window, image=photo, style="Custom.TLabel", relief="flat", borderwidth=0)
+    label1.pack()
+    label1.image = photo
     
     #GENERATE LINK
     label = customtkinter.CTkLabel(window,
